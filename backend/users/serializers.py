03dad21483da4cadf236d02627f28bc5febc9b26
2khs21 <2khs21@naver.com>
2024-08-13 04:22:21 +0900

    Add Login, check return Token

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,6 +5,9 @@ from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token # Token 모델
 from rest_framework.validators import UniqueValidator # 이메일 중복 방지를 위한 도구
+from django.contrib.auth import authenticate
+# Django의 기본 authenticate 함수
+# 우리가 설정한 DefaultAuthBackend인 TokenAuth 방식으로 유저를 인증해줌
 
 class RegisterSerializer(serializers.ModelSerializer):
 		email = serializers.EmailField(
@@ -48,3 +51,32 @@ class RegisterSerializer(serializers.ModelSerializer):
 			# DRF의 기본 인증 방법 중 하나인 토큰 인증을 사용하기 위해 토큰을 생성합니다. 이 토큰은 이후 API 요청 시 사용자가 인증할 때 사용됩니다.
 			# 새로 생성된 Token 인스턴스의 user 필드에 user 변수로 전달된 사용자 객체를 할당합니다.
 			return user
+		
+# class LoginSerializer(serializers.Serializer):
+# 		username = serializers.CharField(required=True)
+# 		password = serializers.CharField(required=True, write_only=True)
+# 		# write_only=True : 클라이언트->서버 역직력화는 가능, 서버->클라이언트 방향의 직렬화는 불가능
+		
+# 		def validate(self, data):
+# 			user = authenticate(username=data['username'], password=data['password'])
+# 			if user:
+# 				token = Token.objects.get(user=user) # 토큰에서 유저 찾아 응답
+# 				return token
+# 			else:
+# 				raise serializers.ValidationError(
+# 					{"error":"Unable to log in with provided credentials."}
+# 					)
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if user:
+            token = Token.objects.get(user=user)
+            return {"token": token.key}  # 토큰 키를 딕셔너리로 반환
+        else:
+            raise serializers.ValidationError(
+                {"error": "Unable to log in with provided credentials."}
+            )
