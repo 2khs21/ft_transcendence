6a1b0340bc2d05c12b629131454c21c16c170a35
haelime <gnostalgiclife@gmail.com>
2024-08-21 17:36:37 +0900

    chore: Add 3D.js script to game page

--- frontend/static/html/game.js
+++ frontend/static/html/game.js
@@ -3,5 +3,6 @@ export function renderGame(container) {
   container.innerHTML = `
 			<h2>Game Page</h2>
 			<p>Your game content goes here.</p>
-	`;
+			<script src="/3d.js" type="module"> </script>
+	`; 
 }
